3ec4307c036308a05138c86aee58be42511fc392
DANIEL <147718556+DANI-SHINO@users.noreply.github.com>
2025-07-04 20:42:48 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,26 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/biblioteca_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-super-segura'
+
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql+pymysql://avnadmin:AVNS_xlj5WYOprcR-tFN6xX1@'
+        'mysql-1b6c1203-mirandadiazjesusdaniel-8cf4.k.aivencloud.com:21093/defaultdb'
+    )
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'connect_args': {
+            'ssl': {
+                'ssl_verify_cert': False,     # 👈 evita verificación de cert
+                'ssl_check_hostname': False   # 👈 evita verificación de hostname
+            }
+        }
+    }
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'clave-secreta-super-segura'
 
     MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
     MAIL_PORT = 2525
-    MAIL_USERNAME = '5941fdd25fae2b'
-    MAIL_PASSWORD = 'd4e6b9abc0517e'
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('5941fdd25fae2b')
+    MAIL_PASSWORD = os.environ.get('d4e6b9abc0517e')
 
